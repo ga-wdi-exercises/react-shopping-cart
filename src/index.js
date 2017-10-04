e54815adbb1285eef115b8973d3904d64abdb678
Drew Dandrea <d.dandrea00@gmail.com>
2017-10-04 19:41:50 -0400

    Created State

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class ShoppingCart extends React.Component {
-  render(){
+  constructor() {
+    super()
+    this.state = {
+      cartTotal: 0,
+      itemsCount: 0,
+      estTax: 0,
+      itemPrice1: 27,
+      itemCount1: 0,
+      itemPrice2: 13,
+      itemCount2: 0,
+      wrapPrice: 5
+   }
+ }
+   
+  render() {
     return (
       <div className='cart'>
         <div className='items'>
-          <h2>cart total: <span>$9.38</span></h2>
+          <h2>cart total: <span>${this.state.cartTotal + this.state.estTax}</span></h2>
           <div className='half right'>
-            <a href='#' className='checkout'>i'm ready to checkout</a>
+            <a href='#' className='checkout'>Checkout</a>
           </div>
           <div className='item'>
             <img  src='https://scene7-secure.targetimg1.com/is/image/Target/39598742?wid=90&hei=90' />
@@ -18,44 +32,44 @@ class ShoppingCart extends React.Component {
             </div>
             <div className='half'>
               <span className='circle'>-</span>
-              <span className='pad-left pad-right'>1</span>
+              <span className='pad-left pad-right'>{this.state.itemCount1}</span>
               <span className='circle'>+</span>
-              <strong className='right'>$27.92</strong>
+              <strong className='right'>${this.state.itemPrice1}</strong>
             </div>
             <hr />
             <div>
               <a href='#'>remove</a><br />
               <label>
-                <input type='checkbox'/> wrap it for $5.99
+                <input type='checkbox'/> wrap it for ${this.state.wrapPrice}
               </label>
             </div>
           </div>
           <div className='item'>
             <img  src='https://scene7-secure.targetimg1.com/is/image/Target/12953828?wid=90&hei=90' />
             <div className='half'>
-              Gaiam Kids Yoga Headband - Pink/Blue
+              Stovetop Signature Turkey Stuffing
             </div>
             <div className='half'>
               <span className='circle'>-</span>
-              <span className='pad-left pad-right'>1</span>
+              <span className='pad-left pad-right'>{this.state.itemCount2}</span>
               <span className='circle'>+</span>
-              <strong className='right'>$27.92</strong>
+              <strong className='right'>${this.state.itemPrice2}</strong>
             </div>
             <hr />
             <div>
               <a href='#'>remove</a><br />
               <label>
-                <input type='checkbox'/> wrap it for $5.99
+                <input type='checkbox'/> wrap it for ${this.state.wrapPrice}
               </label>
             </div>
           </div>
         </div>
         <div className='summary'>
-          <h3>order summary</h3>
+          <h3>Order Summary</h3>
           <div>
             <strong>subtotal</strong>
-            <small> (3 items)</small>
-            <strong className='right'>$8.98</strong>
+            <small> ({this.state.itemsCount} items)</small>
+            <strong className='right'>$0</strong>
           </div>
           <div>
             <strong>delivery</strong>
@@ -63,10 +77,10 @@ class ShoppingCart extends React.Component {
           </div>
           <div>
             <strong>estimated tax</strong>
-            <strong className='right'>$0.40</strong>
+            <strong className='right'>${this.state.estTax}</strong>
           </div>
           <hr/>
-          <h3>total <span className='right'>$9.38</span></h3>
+          <h3>total <span className='right'>${this.state.cartTotal + this.state.estTax}</span></h3>
           <hr/>
           <small>cart number: 1016396673255</small>
         </div>
