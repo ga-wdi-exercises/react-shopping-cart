382bec57c71a414c072be2056b9c3bf582a9fd15
melley berhane <mellinaye@gmail.com>
2017-10-04 21:34:11 -0400

    adding event handlers

---
+++ src/Order.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+export default class Order extends Component {
+  render(){
+    return (
+      <div className='summary'>
+        <h3>order summary</h3>
+        <div>
+          <strong>subtotal</strong>
+          <small> (3 items)</small>
+          <strong className='right'>$8.98</strong>
+        </div>
+        <div>
+          <strong>delivery</strong>
+          <strong className='right red'>free</strong>
+        </div>
+        <div>
+          <strong>estimated tax</strong>
+          <strong className='right'>$0.40</strong>
+        </div>
+        <hr/>
+        <h3>total <span className='right'>$9.38</span></h3>
+        <hr/>
+        <small>cart number: 1016396673255</small>
+      </div>
+
+    )
+  }
+}
---
+++ src/ShoppingCart.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import Order from './Order.js'
+import Item from './Item.js'
+
+const itemsInCart = [
+  {
+    name: "Gaiam Kids Yoga Headband - Pink/Blue",
+    cost: 9.38,
+    imgUrl: "https://scene7-secure.targetimg1.com/is/image/Target/39598742?wid=90&hei=90",
+    quantityInCart: 1
+  },
+  {
+    name: "Hamburger Helper",
+    cost: 11.52,
+    imgUrl: "https://scene7-secure.targetimg1.com/is/image/Target/12953828?wid=90&hei=90",
+    quantityInCart: 1
+  }
+]
+
+export default class ShoppingCart extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: itemsInCart,
+      total: 0,
+      tax: .5
+    }
+
+    this.handleItems = this.handleItems.bind(this)
+  }
+handleItems (e) {
+  this.setState({
+    items: e.target.value
+  })
+}
+
+render(){
+  let items = this.state.items.map((item, i)=>{
+    return <Item name={item.name} cost={item.cost} quantityInCart={item.quantityInCart}/>
+
+})
+    return (
+      <div className='cart'>
+        <div className='items'>
+          <h2>cart total: <span>{this.state.total}</span></h2>
+          <div className='half right'>
+            <a href='#' className='checkout'>i'm ready to checkout</a>
+          </div>
+
+          {items}
+
+
+        </div>
+
+        <Order/>
+
+      </div>
+    )
+  }
+}
